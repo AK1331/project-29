6236a50ebd8f8f4a9ee14b706c9b2808df59a435
AK1331 <73995475+AK1331@users.noreply.github.com>
2021-01-22 15:40:10 +0530

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,28 @@
+class Chain{
+    constructor(bodyA,pointB){
+     var options={
+         bodyA: bodyA,
+         pointB: pointB,
+         stiffness:0.04,
+         lenght:10,
+     }
+     this.chain=Constraint.create(options);
+     this.pointB=pointB;
+     World.add(world,this.chain);
+    }
+
+    fly(){
+        this.chain.bodyA = null;
+    }
+    display(){
+        if(this.chain.bodyA){
+        var pointA=this.chain.bodyA.position;
+        var pointB=this.pointB;
+        strokeWeight(5);
+        fill("lightBlue");
+        line(pointA.x,pointA.y,pointB.x,pointB.y);
+        }
+        
+
+    }
+}
